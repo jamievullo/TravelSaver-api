3286653a053c71e4c5edd973ee4a4ffadd3713f6
jamievullo <jvullo@protonmail.com>
2020-03-01 10:41:16 -0500

    Created sessions routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+  resources :users
 
 end
