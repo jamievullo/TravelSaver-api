da1ae97802c135435a918cfeeb0a9d9c2c611515
jamievullo <jvullo@protonmail.com>
2020-03-09 14:15:37 -0400

    Adding custom routes and scrapes for vacation cards.

---
+++ app/controllers/scrape_controller.rb
@@ -0,0 +1,70 @@
+require 'nokogiri'
+require 'open-uri'
+
+class ScrapeController < ApplicationController
+
+    def popular_destinations
+
+        url = 'https://www.townandcountrymag.com/leisure/travel-guide/g17236981/best-places-to-visit-in-april/'
+        doc = Nokogiri::HTML(open(url))
+        # destinations = []
+        places = doc.css('.listicle-slide')
+        destinations = places.map do |place|
+                {
+                title: place.css('.listicle-slide-hed-text').text,
+                image: place.css('source')[1].values[0],
+                description: place.css('p').text
+                }
+            # destinations << destination
+        end
+        render json: {results: destinations}
+    end
+
+    def vacation_packages
+
+    end
+
+    def weekend_getaways
+
+        url = 'https://vacationidea.com/romance/romantic_beach_getaways.html'
+
+        doc = Nokogiri::HTML(open(url))
+        places = doc.css('.slideshow')
+        destinations = places.map do |place|
+            {
+            title: place.css('.slide-title').text,
+            description: place.css('p')[0].text.strip,
+            image: place.css('.slide-image img').attr('src').value
+            }
+            # destinations << destination
+        end
+        render json: {results: destinations}
+    end
+
+    def explore
+
+        url = 'https://www.businessinsider.com/underrated-travel-destinations-to-visit-in-2020-intrepid-travel-2020-2'
+        doc = Nokogiri::HTML(open(url))        
+        
+        places = doc.css('.slide')
+        
+        destinations = places.map do |place|
+            {
+            title: place.css('.slide-title-text').text,
+            image: place.css('.lazy-holder noscript img').attr('src').value,
+            description: place.css('p').text
+            }
+                # destinations << destination
+        end
+        render json: {results: destinations}
+    end
+
+    def tropical
+
+    end
+
+    def adventure
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
+  
   resources :users
+
+  get '/popular_destinations', to: 'scrape#popular_destinations'
+  get '/vacation_packages', to: 'scrape#vacation_packages'
+  get '/weekend_getaways', to: 'scrape#weekend_getaways'
+  get '/explore', to: 'scrape#explore'
+  get '/tropical', to: 'scrape#tropical'
+  get '/adventure', to: 'scrape#adventure'
 
 end
