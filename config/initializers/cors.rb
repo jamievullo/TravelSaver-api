6e8e9b1ec8d8193574e14995ab361189794eae74
jamievullo <jvullo@protonmail.com>
2020-03-30 19:22:55 -0400

    Changing cors.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-        origins '*'
+        origins 'http://www.travelsaver.io/'
         resource '*',
         # resource '*',
         headers: :any,
